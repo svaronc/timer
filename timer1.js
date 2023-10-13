29529e277f20fd14a395788176142c96545e4d14
Sebastian varon <sebastianvaron96@gmail.com>
2023-10-13 13:07:15 +0000

    wrap the code into a function

--- timer1.js
+++ timer1.js
@@ -5,16 +5,20 @@ const args = process.argv
   .map((time) => time * 1000)
   .sort((a, b) => a - b);
 
-if (args.length > 0) {
-  // check the array has values
-  for (let time of args) {
-    //loop each value
-    if (time > 0 && !isNaN(time)) {
-      //check for negative numbers and NaN
-      setTimeout(() => {
-        //set time out to write beep each value in the array
-        process.stdout.write("Beep  ");
-      }, time);
+const timer = function (args) {
+  if (args.length > 0) {
+    // check the array has values
+    for (let time of args) {
+      //loop each value
+      if (time > 0 && !isNaN(time)) {
+        //check for negative numbers and NaN
+        setTimeout(() => {
+          //set time out to write beep each value in the array
+          process.stdout.write("Beep  ");
+        }, time);
+      }
     }
   }
-}
+};
+// calling the function;
+timer(args);
